1235e7f0066954a5c330b86a405dd7c5734998a2
Severus-hugh <1501717160@qq.com>
2022-07-28 09:17:36 +0800

    git test

---
+++ testdemo/src/testdemo.java
@@ -0,0 +1,9 @@
+public class testdemo {
+    public static void main(String[] args) {
+        System.out.println("这是git上传测试");
+        System.out.println("这是git上传测试");
+        System.out.println("这是git上传测试");
+        System.out.println("这是git上传测试");
+        System.out.println("这是git上传测试");
+    }
+}
