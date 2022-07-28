c1e93e5e642e9730ad86d26c29c36e634af378b6
Severus-hugh <1501717160@qq.com>
2022-07-28 11:07:50 +0800

    new code in demo1

--- testdemo/src/demo1.java
+++ testdemo/src/demo1.java
@@ -2,4 +2,8 @@ public class demo1 {
     public static void main(String[] args) {
         System.out.println("demo1");
     }
+
+    public void test1(){
+        System.out.println("push new code from lin");
+    }
 }
